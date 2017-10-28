2939a08dcf3bc6234fa240314d4520dd4ea2e7ce
Aimun Khan <aimun.khan@utexas.edu>
2017-10-28 18:42:36 -0500

    0.02 Started working on index.js

--- app/index.js
+++ app/index.js
@@ -1,3 +1,10 @@
+// index.js
+// Last modified by: Aimun Khan
+// This is basically our main
+// Monitors heartbeat
+// Adding: Polls heartbeat and checks if larger than initial
+
+/**********Set up Stuff**********/
 import { Accelerometer } from "accelerometer";
 import { Barometer } from "barometer";
 import document from "document";
@@ -8,15 +15,15 @@ console.log("App Started");
 let accelData = document.getElementById("accel-data");
 let barData = document.getElementById("bar-data");
 let hrmData = document.getElementById("hrm-data");
-
 let accel = new Accelerometer();
-let bar = new Barometer();
 let hrm = new HeartRateSensor();
 
-accel.start();
-bar.start();
-hrm.start();
+let lastValueTimestamp = Date.now();    // Keep a timestamp of the last reading received. Start when the app is started.
 
+/**********Functions**********/
+// This was written for us - turns data into a string
+// Doesn't log over time so we're gonna rewrite this
+// Probably don't need the stringify stuff
 function refreshData() {
   let data = {
     accel: {
@@ -24,18 +31,55 @@ function refreshData() {
       y: accel.y ? accel.y.toFixed(1) : 0,
       z: accel.z ? accel.z.toFixed(1) : 0
     },
-    bar: {
-      pressure: bar.pressure ? parseInt(bar.pressure) : 0
-    },
     hrm: {
       heartRate: hrm.heartRate ? hrm.heartRate : 0
     }
   };
 
   accelData.innerText = JSON.stringify(data.accel);
-  barData.innerText = JSON.stringify(data.bar);
   hrmData.innerText = JSON.stringify(data.hrm);
 }
 
-refreshData();
-setInterval(refreshData, 1000);
+// This function takes a number of milliseconds and returns a string
+// such as "5min ago".
+// I don't know if we need this
+function convertMsAgoToString(millisecondsAgo) {
+  if (millisecondsAgo < 120*1000) {
+    return Math.round(millisecondsAgo / 1000) + "s ago";
+  }
+  else if (millisecondsAgo < 60*60*1000) {
+    return Math.round(millisecondsAgo / (60*1000)) + "min ago";
+  }
+  else {
+    return Math.round(millisecondsAgo / (60*60*1000)) + "h ago"
+  }
+}
+
+// This function updates the label on the display that shows when data was last updated.
+function updateDisplay() {
+  if (lastValueTimestamp !== undefined) {
+    updatedLabel.innerText = convertMsAgoToString(Date.now() - lastValueTimestamp);
+  }
+}
+
+// Create a new instance of the HeartRateSensor object
+var hrm = new HeartRateSensor();
+
+// Declare an event handler that will be called every time a new HR value is received.
+// Need to modify this, all this does it put it on the display
+hrm.onreading = function() {
+  // Peek the current sensor values
+  console.log("Current heart rate: " + hrm.heartRate);
+  hrLabel.innerText = hrm.heartRate;
+  lastValueTimestamp = Date.now();
+}
+
+
+/**********Main**********/
+// Begin monitoring the sensors
+accel.start();
+hrm.start();
+
+//refreshData();
+setInterval(refreshData, 1000);     // Get new data every second
+setInterval(updateDisplay, 500);    // Update the display every 0.5s
